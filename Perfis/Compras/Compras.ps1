42df69f389218715b9ee8b039d3652a22de84477
Henrique <henrique.zimermann@trust.group>
2024-04-02 17:55:43 -0300

    salvando

---
+++ Perfis/Compras/Compras.ps1
@@ -0,0 +1,26 @@
+$UserId = ""
+$UserEmail = $env:USERNAME + "@trust.group"
+$SiteId = "{b0538071-d508-4de6-a461-54cf8e4816d0}"
+$isSiteAdmin = "1"
+$WebId = "{87bcb753-c805-42d9-bad0-aa9bc035f0e8}"
+$WebTitle = ""
+$WebTemplate = "64"
+$WebLogoUrl = "/sites/TrustGroup19/_api/GroupService/GetGroupImage?id='d22aa4cd-10b7-4dbb-9ff0-011c010f5f2e'&hash=638334216242572433"
+$webUrl = "https://trustlog.sharepoint.com/sites/TrustGroup19"
+$onPrem = "0"
+$LibraryType = "3"
+$ListId = "639c425a-8102-4d1f-b763-c51cc006c833"
+$ListTitle = "Compras"
+$Scope = "OPENLIST"
+
+Do {
+    $ODStatus = Get-Process onedrive -ErrorAction SilentlyContinue
+   
+    If ($ODStatus) {
+ 
+        $odopen = "odopen://sync/?userId=" + $UserId + "&userEmail=" + $UserEmail + "&isSiteAdmin=" + $isSiteAdmin + "&siteId=" + $SiteId + "&webId=" + $WebId + "&webTitle=" + $WebTitle + "&webTemplate=" + $WebTemplate + "&webLogoUrl=" + $WebLogoUrl + "&webUrl=" + $webUrl + "&onPrem=" + $onPrem + "&libraryType=" + $LibraryType + "&listId=" + $ListId + "&listTitle=" + $ListTitle + "&scope=" + $Scope
+
+        Start-Process $odopen
+    }
+}
+Until ($ODStatus)
---
+++ Perfis/Marketing/ADMVendas&Marketing.ps1
@@ -0,0 +1,29 @@
+$UserId = ""
+$UserEmail = $env:USERNAME + "@trust.group"
+$SiteId = "{b0538071-d508-4de6-a461-54cf8e4816d0}"
+$isSiteAdmin = "1"
+$WebId = "{87bcb753-c805-42d9-bad0-aa9bc035f0e8}"
+$WebTitle = ""
+$WebTemplate = "64"
+$WebLogoUrl = "/sites/TrustGroup19/_api/GroupService/GetGroupImage?id='d22aa4cd-10b7-4dbb-9ff0-011c010f5f2e'&hash=638334216242572433"
+$webUrl = "https://trustlog.sharepoint.com/sites/TrustGroup19"
+$onPrem = "0"
+$LibraryType = "3"
+$ListId = "dfa5c48b-8f10-4b24-a251-03be8c58ed45"
+$ListTitle = ""
+$FolderId = "d585a539-fa83-431c-bc86-f965e405cdc1"
+$FolderName = "ADM VENDAS & MARKETING"
+$FolderUrl = "https://trustlog.sharepoint.com/sites/TrustGroup19/Marketing/ADM%20VENDAS%20&%20MARKETING"
+$Scope = "OPENFOLDER"
+
+Do {
+    $ODStatus = Get-Process onedrive -ErrorAction SilentlyContinue
+   
+    If ($ODStatus) {
+ 
+        $odopen = "odopen://sync/?userId=" + $UserId + "&userEmail=" + $UserEmail + "&isSiteAdmin=" + $isSiteAdmin + "&siteId=" + $SiteId + "&webId=" + $WebId + "&webTitle=" + $WebTitle + "&webTemplate=" + $WebTemplate + "&webLogoUrl=" + $WebLogoUrl + "&webUrl=" + $webUrl + "&onPrem=" + $onPrem + "&libraryType=" + $LibraryType + "&listId=" + $ListId + "&listTitle=" + $ListTitle + "&folderId=" + $FolderId + "&folderName=" + $FolderName + "&folderUrl=" + $FolderUrl + "&scope=" + $Scope
+
+        Start-Process $odopen
+    }
+}
+Until ($ODStatus)
